b39d7a12b0ac7c48a156005241c9806318fba93b
Kyaw Zin Hein <kyawzinhein6@gmail.com>
2020-12-06 18:48:53 +0630

    sort open hours by start time

--- backend/controllers/openHourController.js
+++ backend/controllers/openHourController.js
@@ -1,11 +1,8 @@
 const OpenHour = require('../models/openHourModel');
 const catchAsync = require('../utils/catchAsync');
-const AppError = require('../utils/appError');
-const APIFeatures = require('../utils/apiFeatures');
-const getDay = require('../utils/getDay');
 
 exports.getAllOpenHours = catchAsync(async (req, res, next) => {
-  const openHours = await OpenHour.find();
+  const openHours = await OpenHour.find().sort('start');
 
   res.status(200).json({
     status: 'success',
